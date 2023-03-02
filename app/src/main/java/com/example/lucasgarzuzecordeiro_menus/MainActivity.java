9d41271c0de291741f816bde1e028c52f7d90643
lucas_garzuze_cordeiro <lucasgarzuze@gmail.com>
2023-03-02 11:48:38 -0300

    Adionando menus de novo jogo e carregar jogo

--- app/src/main/java/com/example/lucasgarzuzecordeiro_menus/MainActivity.java
+++ app/src/main/java/com/example/lucasgarzuzecordeiro_menus/MainActivity.java
@@ -18,16 +18,28 @@ Button btnSobre;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         btnLogin=findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener((View.OnClickListener) view -> {
             Intent in = new Intent(MainActivity.this, Login.class);
             startActivity(in);
         });
 
-                btnLogin=findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(view -> {
-            Intent in = new Intent(MainActivity.this, Login.class);
+        btnNovoJogo=findViewById(R.id.btnNovoJogo);
+        btnNovoJogo.setOnClickListener(view -> {
+            Intent in = new Intent(MainActivity.this, NovoJogo.class);
+            startActivity(in);
+        });
+
+        btnCarregarJogo=findViewById(R.id.btnCarregarJogo);
+        btnCarregarJogo.setOnClickListener((View.OnClickListener) view -> {
+            Intent in = new Intent(MainActivity.this, CarregarJogo.class);
+            startActivity(in);
+        });
+
+        btnNovoJogo=findViewById(R.id.btnNovoJogo);
+        btnNovoJogo.setOnClickListener(view -> {
+            Intent in = new Intent(MainActivity.this, NovoJogo.class);
             startActivity(in);
         });
     }}
