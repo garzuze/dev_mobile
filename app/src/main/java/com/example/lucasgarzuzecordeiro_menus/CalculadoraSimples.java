4c408089d81f509c094c9f21fccc5f3d30b6198f
lucas-garzuze-cordeiro <lucasgarzuze@gmail.com>
2023-03-16 12:42:07 -0300

    Calculadora simples terminada Terminei a janela da calculadora simples! Coloquei operações de adição, subtraçao, multiplicação e divisão.

--- app/src/main/java/com/example/lucasgarzuzecordeiro_menus/CalculadoraSimples.java
+++ app/src/main/java/com/example/lucasgarzuzecordeiro_menus/CalculadoraSimples.java
@@ -8,8 +8,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class CalculadoraSimples extends AppCompatActivity {
-Button btnCalcular;
+Button btnSoma, btnSubtracao, btnDivisao, btnMultiplicacao;
 TextView txtVisorCalculadoraSimples;
+TextView txtVisorCalculadoraSimples2;
+TextView txtResultado;
 Double num1, num2, num3;
 
     @Override
@@ -17,11 +19,54 @@ Double num1, num2, num3;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora_simples);
 
-        btnCalcular=findViewById(R.id.btnCalcular);
+        btnSoma=findViewById(R.id.btnSoma);
+        btnSubtracao=findViewById(R.id.btnSubtracao);
+        btnDivisao=findViewById(R.id.btnDivisao);
+        btnMultiplicacao=findViewById(R.id.btnMultiplicacao);
         txtVisorCalculadoraSimples=findViewById(R.id.txtVisorCalculadoraSimples);
-        btnCalcular.setOnClickListener((View.OnClickListener) view -> {
-            num1 = Double.parseDouble(txtVisorCalculadoraSimples.getText().toString());
-            txtVisorCalculadoraSimples.setText("");
+        txtVisorCalculadoraSimples2=findViewById(R.id.txtVisorCalculadoraSimples2);
+        txtResultado=findViewById(R.id.txtResultado);
+        btnSoma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtVisorCalculadoraSimples.getText().toString());
+                num2 = Double.parseDouble(txtVisorCalculadoraSimples2.getText().toString());
+                double resultado = num1 + num2;
+                String resultadoString = String.valueOf(resultado);
+                txtResultado.setText(resultadoString);
+            }
+        });
+        btnSubtracao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtVisorCalculadoraSimples.getText().toString());
+                num2 = Double.parseDouble(txtVisorCalculadoraSimples2.getText().toString());
+                double resultado = num1 - num2;
+                String resultadoString = String.valueOf(resultado);
+                txtResultado.setText(resultadoString);
+            }
+        });
+
+        btnDivisao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtVisorCalculadoraSimples.getText().toString());
+                num2 = Double.parseDouble(txtVisorCalculadoraSimples2.getText().toString());
+                double resultado = num1 / num2;
+                String resultadoString = String.valueOf(resultado);
+                txtResultado.setText(resultadoString);
+            }
+        });
+
+        btnMultiplicacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtVisorCalculadoraSimples.getText().toString());
+                num2 = Double.parseDouble(txtVisorCalculadoraSimples2.getText().toString());
+                double resultado = num1 * num2;
+                String resultadoString = String.valueOf(resultado);
+                txtResultado.setText(resultadoString);
+            }
         });
 
     }
