87d4e63bf7d06c67d3cdfc2979fad92de40137e0
Lucas Garzuze Cordeiro <118022446+lucas-garzuze-cordeiro@users.noreply.github.com>
2023-03-23 11:36:14 -0300

    Criando nova calculadora Agora o objetivo é criar uma calculadora que pega o resultado matemático e exibe em outra tela

---
+++ app/src/main/java/com/example/lucasgarzuzecordeiro_menus/Calculadora2.java
@@ -0,0 +1,63 @@
+package com.example.lucasgarzuzecordeiro_menus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Calculadora2 extends AppCompatActivity {
+    Button btnSoma2 = findViewById(R.id.btnSoma2), btnSubtracao2 = findViewById(R.id.btnSubtracao2), btnDivisao2 = findViewById(R.id.btnDivisao2), btnMultiplicacao2 = findViewById(R.id.btnMultiplicacao2);
+    TextView txtNumero1 = findViewById(R.id.txtNumero1), txtNumero2 = findViewById(R.id.txtNumero2);
+    TextView txtResultado2 = findViewById(R.id.txtResultado2);
+    Double num1, num2, num3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora2);
+        btnSoma2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtNumero1.getText().toString());
+                num2 = Double.parseDouble(txtNumero2.getText().toString());
+                double resultado = num1 + num2;
+                String resultadoString = String.valueOf(resultado);
+                txtResultado2.setText(resultadoString);
+            }
+        });
+        btnSubtracao2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtNumero1.getText().toString());
+                num2 = Double.parseDouble(txtNumero2.getText().toString());
+                double resultado = num1 - num2;
+                String resultadoString = String.valueOf(resultado);
+                txtResultado2.setText(resultadoString);
+            }
+        });
+
+        btnDivisao2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtNumero1.getText().toString());
+                num2 = Double.parseDouble(txtNumero2.getText().toString());
+                double resultado = num1 / num2;
+                String resultadoString = String.valueOf(resultado);
+                txtResultado2.setText(resultadoString);
+            }
+        });
+
+        btnMultiplicacao2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtNumero1.getText().toString());
+                num2 = Double.parseDouble(txtNumero2.getText().toString());
+                double resultado = num1 * num2;
+                String resultadoString = String.valueOf(resultado);
+                txtResultado2.setText(resultadoString);
+            }
+        });
+    }
+}
